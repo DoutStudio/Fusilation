fd65a0ed72ce2b7773ff5d2a6959352cb46c10e9
Ryan Scopio <ryanscopio@gmail.com>
2016-09-09 15:05:03 -0700

    Added Stars & Fixed ship prefabs
    todo: fix star brightness, they are too dim

---
+++ Assets/Scripts/DebugCameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugCameraMovement : MonoBehaviour
+{
+
+    public float speed = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Flicker : MonoBehaviour
+{
+
+    public float minimumFlickerSpeed = 0.3f;
+    public float maximumFlickerSpeed = 1f;
+    private float flickerSpeed;
+    public Color startColor = Color.white;
+    public Color endColor = Color.black;
+    private SpriteRenderer render;
+    private float elapsedTime = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        StartCoroutine(Fade());
+        flickerSpeed = Random.Range(minimumFlickerSpeed, maximumFlickerSpeed);
+    }
+
+    IEnumerator Fade()
+    {
+        while(true)
+        {
+            if(elapsedTime >= flickerSpeed)
+            {
+                elapsedTime -= flickerSpeed;
+                Color temp = startColor;
+                startColor = endColor;
+                endColor = temp;
+            }
+            render.color = Color.Lerp(startColor, endColor, elapsedTime / flickerSpeed);
+            elapsedTime += 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public void OnBecameInvisible()
+    {
+        StopCoroutine(Fade());
+    }
+
+    public void OnBecameVisible()
+    {
+        StartCoroutine(Fade());
+    }
+}
