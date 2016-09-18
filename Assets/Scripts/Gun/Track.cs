74c116e56f020de6b0f03c35309a5127114f9a64
Ryan Scopio <ryanscopio@gmail.com>
2016-09-18 02:30:05 -0700

    Predictive Aiming now works! WARNING: heavy cleanup required. I don't need as many variables and objects that I used.
    Ship movement updated: Can now use physics to move between multiple points
    Ship Prefab updated: polygon collider now manually formed to parent object to be next to the rigidbody (used to be multiple polygon colliders for each object)

--- Assets/Scripts/Gun/Track.cs
+++ Assets/Scripts/Gun/Track.cs
@@ -6,6 +6,7 @@ public class Track : MonoBehaviour
 
     public Transform Target;
     public float TurnSpeed = 5;
+    public Transform interceptionPoint;
 
     Rigidbody2D targetBody;
     GunController gun;
@@ -25,14 +26,23 @@ public class Track : MonoBehaviour
         if (Target)
         { // enemy alive and at sight: aim at him!
 
-            transform.Rotate(transform.forward, CalculateIntercourse(Target.position, targetBody.velocity, transform.position, gun.Power));
+            //use target velocity once ship has regular movement
+            Vector3 IC = CalculateInterceptCourse(targetBody.position, targetBody.velocity, transform.position, gun.Power);
+            if (IC != Vector3.zero)
+            {
+                IC.Normalize();
+                float interceptionTime = FindClosestPointOfApproach(targetBody.position, targetBody.velocity, transform.position, IC * gun.Power);
+                interceptionPoint.position = targetBody.position + targetBody.velocity * interceptionTime;
+            }
 
-            /*
-            Vector3 dir = CalculateIntercourse(Target.position, targetBody.velocity, transform.position, gun.Power);
+            Transform aimTarget = interceptionPoint;
+            //transform.LookAt(aimTarget, transform.up);
 
-            Quaternion rot = Quaternion.FromToRotation(Vector3.up, dir);
+
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, aimTarget.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, rot, TurnSpeed * Time.deltaTime);
-            */
+            
+
             // you may shoot here
         }
         else
@@ -42,17 +52,55 @@ public class Track : MonoBehaviour
         }
     }
 
-    public static float CalculateIntercourse(Vector3 targetPos, Vector3 targetSpeed, Vector3 turretPos, float bulletSpeed)
+    public static Vector3 CalculateInterceptCourse(Vector3 aTargetPos, Vector3 aTargetSpeed, Vector3 aInterceptorPos, float aInterceptorSpeed)
     {
-        Vector3 diff = Vector3.Normalize(targetPos - turretPos);
-        Vector3 diffJ = diff * Vector3.Dot(diff, targetSpeed);
-        Vector3 diffU = targetSpeed - diffJ;
-        float magNorm = Mathf.Sqrt(Mathf.Pow(bulletSpeed, 2) - Mathf.Pow(diffU.magnitude, 2));
-        Vector3 mag = diff * magNorm;
-        Vector3 attackVector = diffU + mag;
+        Vector3 targetDir = aTargetPos - aInterceptorPos;
 
-        float angle = Mathf.Acos((Vector3.Dot(diff, attackVector) / (diff.magnitude * attackVector.magnitude)));
+        float iSpeed2 = aInterceptorSpeed * aInterceptorSpeed;
+        float tSpeed2 = aTargetSpeed.sqrMagnitude;
+        float fDot1 = Vector3.Dot(targetDir, aTargetSpeed);
+        float targetDist2 = targetDir.sqrMagnitude;
+        float d = (fDot1 * fDot1) - targetDist2 * (tSpeed2 - iSpeed2);
+        if (d < 0.1f)  // negative == no possible course because the interceptor isn't fast enough
+            return Vector3.zero;
+        float sqrt = Mathf.Sqrt(d);
+        float S1 = (-fDot1 - sqrt) / targetDist2;
+        float S2 = (-fDot1 + sqrt) / targetDist2;
 
-        return angle;
+        if (S1 < 0.0001f)
+        {
+            if (S2 < 0.0001f)
+                return Vector3.zero;
+            else
+                return (S2) * targetDir + aTargetSpeed;
+        }
+        else if (S2 < 0.0001f)
+            return (S1) * targetDir + aTargetSpeed;
+        else if (S1 < S2)
+            return (S2) * targetDir + aTargetSpeed;
+        else
+            return (S1) * targetDir + aTargetSpeed;
+    }
+
+
+    /// <summary>
+    /// Calculates the time when the two given objects reach their closest point of approach
+    /// </summary>
+    /// <param name="aPos1">the position of object1</param>
+    /// <param name="aSpeed1">the velocity vector of object1 in u/s</param>
+    /// <param name="aPos2">the position of object2</param>
+    /// <param name="aSpeed2">the velocity vector of object2 in u/s</param>
+    /// <returns>the time in seconds when the two objects come closest</returns>
+    public static float FindClosestPointOfApproach(Vector3 aPos1, Vector3 aSpeed1, Vector3 aPos2, Vector3 aSpeed2)
+    {
+        Vector3 PVec = aPos1 - aPos2;
+        Vector3 SVec = aSpeed1 - aSpeed2;
+        float d = SVec.sqrMagnitude;
+        // if d is 0 then the distance between Pos1 and Pos2 is never changing
+        // so there is no point of closest approach... return 0
+        // 0 means the closest approach is now!
+        if (d >= -0.0001f && d <= 0.0002f)
+            return 0.0f;
+        return (-Vector3.Dot(PVec, SVec) / d);
     }
 }
