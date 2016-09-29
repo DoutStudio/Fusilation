05d5e853001d81a03a91eb282932e9df635c4d36
Samson Tse <sammy_tse@msn.com>
2016-09-29 09:52:34 -0700

    Added battleLoading Screen, filled in all remaining shipSprites for Leif (all undone ones are green Octagons)

---
+++ Assets/Scripts/ModuleSelection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ModuleSelection : MonoBehaviour {
+
+    private Text popup = null;
+    private DescriptionScript descriptionS;
+    private string descriptionText;
+
+	// Use this for initialization
+	void Start () {
+        string currentObject = this.gameObject.name;
+        popup = GameObject.Find("messageBoxText").GetComponent<Text>();
+        descriptionS = GameObject.Find(currentObject).GetComponent<DescriptionScript>();
+        descriptionText = descriptionS.description;
+	}
+
+    void Update()
+    {
+    }
+
+    public void ChangeText (Text popUp, string moduleDescription)
+    {
+        popUp.text = moduleDescription;
+    }
+
+    void OnMouseDown()
+    {
+        ChangeText(popup, descriptionText);
+    }
+}
