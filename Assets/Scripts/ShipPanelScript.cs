1c31e8071b3e1b99f495acafe5b220e651da744b
Leif Myer <leifmyer@gmail.com>
2016-09-29 11:51:13 -0700

    fixed items being added twice

--- Assets/Scripts/ShipPanelScript.cs
+++ Assets/Scripts/ShipPanelScript.cs
@@ -8,6 +8,8 @@ public class ShipPanelScript : MonoBehaviour
 {
     // A list of all ship modules in this panel
     public GameObject[] listItemModules;
+    public GameObject listItem;
+
 
     private GameObject statPanel;
 
@@ -15,13 +17,12 @@ public class ShipPanelScript : MonoBehaviour
 	void Start () {
         //listItemModules = Resources.LoadAll<GameObject>("Attack");
         statPanel = GameObject.Find("ShipStatsPanel");
-        GameObject childItem = transform.GetChild(0).gameObject;
 
         for (int i = 0; i < listItemModules.Length; ++i)
         {
-            childItem.GetComponent<ModuleThumbnailScript>().shipModule = listItemModules[i];
-            childItem.GetComponent<ModuleThumbnailScript>().shipStatPanel = statPanel;
-            GameObject newItem = Instantiate(childItem);
+            GameObject newItem = Instantiate(listItem);
+            newItem.GetComponent<ModuleThumbnailScript>().shipModule = listItemModules[i];
+            newItem.GetComponent<ModuleThumbnailScript>().shipStatPanel = statPanel;
             newItem.transform.SetParent(transform);
         }
 
