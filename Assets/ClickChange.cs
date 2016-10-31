5fb0cd2a992bc7d0df83e913739825338556da4c
Leif Myer <leifmyer@gmail.com>
2016-10-30 22:29:18 -0700

    Finished the click change and message capabilities, now latchslotloads can be placed on any object and will change to the item specified as well as display their description if clicked on -- known bug adding the captains to the slots will result in an image too big

--- Assets/ClickChange.cs
+++ Assets/ClickChange.cs
@@ -10,35 +10,46 @@ public class ClickChange : MonoBehaviour {
 
     //public Sprite spriteToSelect;
     SpriteRenderer render;
-    Sprite tempSprite;
     public GameObject tempGO;
     public Text textBoxToChange;
-    private GUIText[] allTextBoxes;
     private GameObject dunno;
-    public int wtf;
-    SpriteRenderer renderer;
+    public int idNumber;
+    public bool grabName;
+    public bool isEnemy;
+    public bool isCaptain;
 
     // Use this for initialization
     void Start () {
         render = GetComponent<SpriteRenderer>();
+        Transform currentObjectSize = GetComponent<Transform>();
         //Debug.Log(Path.GetDirectoryName("/Assets/Prefabs/Ship Modules/Attack/BasicTurret.prefab"));
 		//GameObject prefab = Resources.Load<GameObject>("Ship Modules/Attack/BasicTurret");
 		//Instantiate (prefab);
 		//PLs or y
-		GameObject fml = new GameObject();
-		renderer = fml.AddComponent<SpriteRenderer> ();
-		renderer.sprite = Resources.Load("Sprites/BasicTurret", typeof(Sprite)) as Sprite;
-		render.sprite = renderer.sprite;
+		//GameObject fml = new GameObject();
+		//renderer = fml.AddComponent<SpriteRenderer> ();
+		//renderer.sprite = Resources.Load("Sprites/BasicTurret", typeof(Sprite)) as Sprite;
+		//render.sprite = renderer.sprite;
+
+        Texture2D fuckBoats = AssetPreview.GetAssetPreview(tempGO);
+        //fuckBoats
+
 
 
         //dunno = Resources.Load<GameObject>("Ship Modules\\Attack\\BasicTurret");
-        dunno = (GameObject)Instantiate(tempGO, render.transform.position, Quaternion.identity);
-
-        Texture2D fuckBoats = AssetPreview.GetAssetPreview(tempGO);
-
-        dunno.GetComponent<Image>().sprite =
-            Sprite.Create(fuckBoats, new Rect(0, 0, 128, 128), Vector2.zero);
-        GetComponent<Collider>().gameObject.name = dunno.gameObject.name;
+        
+        dunno = (GameObject)Instantiate(tempGO, render.transform, false);
+        dunno.gameObject.name = "Module#" + idNumber;
+        dunno.transform.localScale = new Vector3(1, 1, 1);
+        dunno.transform.localScale = new Vector3(currentObjectSize.localScale.x,
+            currentObjectSize.localScale.y, currentObjectSize.localScale.z);
+        //dunno.AddComponent<SpriteRenderer>();
+        //render.sprite = dunno.GetComponent<SpriteRenderer>().sprite;
+        //Sprite tempSprite = Sprite.Create(fuckBoats, new Rect(0, 0, fuckBoats.width, fuckBoats.height), Vector2.zero);
+        //dunno.GetComponent<SpriteRenderer>().sprite = tempSprite;
+        //dunno.transform.SetParent(render.transform);
+        
+        //GetComponent<Collider>().gameObject.name = dunno.gameObject.name;
 
         //Destroy(render.gameObject);
         /*allTextBoxes = FindObjectsOfType<GUIText>();
@@ -64,12 +75,15 @@ public class ClickChange : MonoBehaviour {
 
         //GameObject hello = Instantiate (prefab);
         //hello.transform.position = 	new Vector3 (render.transform.position.x, render.transform.position.y, 0);
-        Destroy (render.gameObject);
+        //dunno.transform.SetParent(dunno.transform);
+        render.enabled = false;
+        //Destroy (render.gameObject);
         //Debug.Log (renderer2.sprite);
     }
 	
 	// Update is called once per frame
-	void Update () { 
+	void Update () {
+        //dunno.transform.localScale = new Vector3(.5f, .5f, 1);
         if (Input.GetMouseButtonDown(0))
         { // if left button pressed...
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -77,8 +91,21 @@ public class ClickChange : MonoBehaviour {
             bool didItHitACollider = Physics.Raycast(ray, out hit);
             if (didItHitACollider && hit.collider.gameObject.name == dunno.gameObject.name)
             {
-                //textBoxToChange.text = dunno.GetComponent<DescriptionScript>().description;
-                textBoxToChange.text = dunno.gameObject.name;
+                if (!grabName)
+                {
+                    textBoxToChange.text = dunno.GetComponent<DescriptionScript>().description;
+                }
+                else
+                {
+                    if (isEnemy)
+                    {
+                        textBoxToChange.text = dunno.gameObject.name + " has been targeted";
+                    }
+                    else
+                    {
+                        textBoxToChange.text = dunno.gameObject.name + " has been selected for repair";
+                    }
+                }
             }
         }
     }
