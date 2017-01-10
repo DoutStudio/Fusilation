4166484807f347a21de81bc8285bda35659a5c00
Ryan Scopio <ryanscopio@gmail.com>
2017-01-09 21:11:00 -0800

    Added a simple singleton to the Ship loader object in Battle

---
+++ Assets/Scripts/SimpleSingleton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleSingleton : MonoBehaviour
+{
+
+    private static SimpleSingleton instance = null;
+
+    public static SimpleSingleton Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        Debug.Log("SINGLETON UPDATE");
+    }
+}
